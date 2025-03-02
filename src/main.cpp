98d1476723e79d1742f56e1d7539b1e0309d9796
Francesco de Gioia <francescodg4@gmail.com>
2025-03-02 17:47:53 +0100

    Add command line options and screenshot mode

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,26 @@
 #include "Theme.hpp"
 
 #include <QApplication>
+#include <QCommandLineParser>
+#include <QDir>
+#include <QTimer>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
     QApplication::setApplicationName(QStringLiteral("PDA"));
+    QApplication::setApplicationVersion(QStringLiteral(PDA_VERSION));
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QStringLiteral("Holographic PDA interface demo"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+    const QCommandLineOption photosOption("photos", "Folder with the acquired images.", "dir", QStringLiteral(PDA_DEFAULT_PHOTO_DIR));
+    const QCommandLineOption pageOption("page", "Page shown at start (1-6).", "n", "1");
+    const QCommandLineOption fullScreenOption("fullscreen", "Run full screen (F11 toggles).");
+    const QCommandLineOption screenshotOption("screenshot", "Save every page as page-N.png in <dir> and quit.", "dir");
+    parser.addOptions({ photosOption, pageOption, fullScreenOption, screenshotOption });
+    parser.process(app);
 
     QApplication::setStyle(QStringLiteral("Fusion"));
     app.setStyleSheet(Theme::styleSheet());
@@ -15,9 +30,30 @@ int main(int argc, char* argv[])
     font.setPointSizeF(10);
     QApplication::setFont(font);
 
-    PhotoLibrary photos(QStringLiteral(PDA_DEFAULT_PHOTO_DIR));
+    PhotoLibrary photos(parser.value(photosOption));
     PdaWindow window(&photos);
     window.resize(1100, 640);
-    window.show();
+    window.setPage(parser.value(pageOption).toInt() - 1);
+
+    if (parser.isSet(screenshotOption)) {
+        const QDir dir(parser.value(screenshotOption));
+        dir.mkpath(QStringLiteral("."));
+        window.show();
+        QTimer::singleShot(300, &window, [&] {
+            for (int i = 0; i < window.pageCount(); ++i) {
+                window.setPage(i);
+                QApplication::processEvents();
+                window.grab().save(dir.filePath(QStringLiteral("page-%1.png").arg(i + 1)));
+            }
+            QApplication::quit();
+        });
+        return app.exec();
+    }
+
+    if (parser.isSet(fullScreenOption)) {
+        window.showFullScreen();
+    } else {
+        window.show();
+    }
     return app.exec();
 }
